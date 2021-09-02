cedc3dfec44c322797b0e542e69e71a2e7286aef
Alvin Zheng <47834226+alvinzhengq@users.noreply.github.com>
2021-09-02 09:01:16 -0700

    New Team Page (#1)
    * New team page.
    
    * Added team name to README
    
    * Added team members to README
    
    * Added team bios to greetings page.
    
    * akshay bio
    
    * Update Greet.Java
    
    Add bio about Nolan Platt
    
    * Update Greet.java
    
    * Update Greet.java
    
    * Update Greet.java
    
    Fix typing error
    
    * Update Greet.java
    
    * Alvin's Bio
    
    * Make greeting page instructions more specific
    
    Co-authored-by: Akshay-Rohatgi <awsm3akshay@gmail.com>
    Co-authored-by: Nolan Platt <43970139+nolanplatt@users.noreply.github.com>
    Co-authored-by: Prisha Maiti <89211997+PrishaMaiti@users.noreply.github.com>
    Co-authored-by: SamiDeshatty <43257337+SamiDeshatty@users.noreply.github.com>
    Co-authored-by: Akshay Rohatgi <52616034+Akshay-Rohatgi@users.noreply.github.com>

--- src/main/java/com/example/sping_portfolio/controllers/Greet.java
+++ src/main/java/com/example/sping_portfolio/controllers/Greet.java
@@ -8,12 +8,31 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Controller  // HTTP requests are handled as a controller, using the @Controller annotation
 public class Greet {
-    @GetMapping("/greet")    // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        // @RequestParam handles required and default values, name and model are class variables, model looking like JSON
-        model.addAttribute("name", name); // MODEL is passed to html
+    private static Map<String, String> ourBios = Map.of(
+        "Alvin", "Hello, my name is Alvin and I am a junior at Del Norte. Outside of school, I enjoy working with computers, biking, and video games.",
+        "Akshay", "Hi! I'm Akshay and I'm a junior at Del Norte! My Interests include playing with computers, music, biking, and watching anime.",
+        "Nolan", "Hi, I'm a junior at Del Norte High School and I am interested in CS, aerospace, and mathematics. I've been programming for almost six years and it's one of my biggest hobbies.",
+        "Sami", "Hey! I'm a junior at Del Norte High School. I enjoy dancing and listening to music in my free time.  My favorite subject at school is Science and I'm interested in genonimcal sciences.",
+        "Prisha", "I'm a junior at Del Norte High School taking APCSA. I enjoy spending my free time hanging out with friends, listening to music, and playing video games."
+    );
+
+    @GetMapping("/greet")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name,
+                           @RequestParam(name="data", required = false, defaultValue = "") String data,
+                           Model model) {
+
+        model.addAttribute("name", name);
+        if(ourBios.containsKey(name)) {
+            model.addAttribute("data", ourBios.get(name));
+        } else {
+            model.addAttribute("data", "No Bio Found.");
+        }
+
         return "greet"; // returns HTML VIEW (greeting)
     }
 }
