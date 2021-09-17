073bf156982aaf9a6a5ec2e92a9d4b03ed15fc5e
Akshay-Rohatgi <awsm3akshay@gmail.com>
2021-09-17 09:02:30 -0700

    make sure bits variable defined in controller

--- src/main/java/com/example/sping_portfolio/controllers/Binary.java
+++ src/main/java/com/example/sping_portfolio/controllers/Binary.java
@@ -8,9 +8,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class Binary {
     String imageOffPath = "/images/bulb_off.png";
     String imageOnPath = "/images/bulb_on.gif";
-
+    int bits = 8;
     @GetMapping("/binary")
     public String Binary(Model model) {
+        model.addAttribute( "bits", bits);
         model.addAttribute("imageOffPath", imageOffPath);
         model.addAttribute("imageOnPath", imageOnPath);
         return "binary";
