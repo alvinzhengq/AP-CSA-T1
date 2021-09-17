63d0ae589bcc7824a2d3de759282814665454588
Akshay-Rohatgi <awsm3akshay@gmail.com>
2021-09-17 08:58:15 -0700

    Hack 1 for binary coding task

--- src/main/java/com/example/sping_portfolio/controllers/Binary.java
+++ src/main/java/com/example/sping_portfolio/controllers/Binary.java
@@ -1,12 +1,18 @@
 package com.example.sping_portfolio.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class Binary {
+    String imageOffPath = "/images/bulb_off.png";
+    String imageOnPath = "/images/bulb_on.gif";
+
     @GetMapping("/binary")
-    public String Binary() {
+    public String Binary(Model model) {
+        model.addAttribute("imageOffPath", imageOffPath);
+        model.addAttribute("imageOnPath", imageOnPath);
         return "binary";
     }
 }
