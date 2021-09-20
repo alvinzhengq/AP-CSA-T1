7b11212cb573a37aae2281f1c0ebb64258ac13ba
alvinzhengq <vortexdomainia@gmail.com>
2021-09-20 00:53:00 -0700

    added hack 2 3 and 4

--- src/main/java/com/example/sping_portfolio/controllers/Binary.java
+++ src/main/java/com/example/sping_portfolio/controllers/Binary.java
@@ -3,14 +3,15 @@ package com.example.sping_portfolio.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class Binary {
     String imageOffPath = "/images/bulb_off.png";
     String imageOnPath = "/images/bulb_on.gif";
-    int bits = 8;
+
     @GetMapping("/binary")
-    public String Binary(Model model) {
+    public String Binary(Model model, @RequestParam(name="bits", required=false, defaultValue="8") int bits) {
         model.addAttribute( "bits", bits);
         model.addAttribute("imageOffPath", imageOffPath);
         model.addAttribute("imageOnPath", imageOnPath);
