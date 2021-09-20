04fc1157aa51b37b388c2c1701769ae443c90f7d
Nolan Platt <plattnolan@gmail.com>
2021-09-20 08:56:30 -0700

    Fix 404 on Individual Minilabs
    Patched issue when using navbar to get to individual minilab.

--- src/main/java/com/example/sping_portfolio/controllers/NolanRGBLab.java
+++ src/main/java/com/example/sping_portfolio/controllers/NolanRGBLab.java
@@ -3,11 +3,13 @@ package com.example.sping_portfolio.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class NolanRGBLab {
-        @GetMapping("/NolanRGBLab/")
-        public String team() {
-            return "NolanRGBLab"; // returns HTML VIEW (greeting)
-        }
+
+    @GetMapping("/NolanRGBLab")
+    public String getNolanRGBLab() {
+        return "NolanRGBLab";
+    }
     }
 
 
