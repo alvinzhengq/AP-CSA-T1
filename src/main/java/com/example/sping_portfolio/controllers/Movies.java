d6ad7e79d3103332752b1a720949deae55ea3b5a
Nolan Platt <plattnolan@gmail.com>
2021-10-27 09:12:42 -0700

    Movie Page Updates
    updates

--- src/main/java/com/example/sping_portfolio/controllers/Movies.java
+++ src/main/java/com/example/sping_portfolio/controllers/Movies.java
@@ -17,6 +17,8 @@ import java.util.Scanner;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.json.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 
@@ -29,10 +31,11 @@ public class Movies {
         return "mvfetch/movies";
     }
 
-    @GetMapping("/mvfetch/movies-api")
+    @PostMapping(value = "/mvfetch/movies-api", consumes = "text/plain")
     @ResponseBody
-    public String[] movieAPI() throws Exception {
-        ArrayList<String> titles = getTitles();
+    public String[] movieAPI(@RequestBody String genresToFilter) throws Exception {
+        System.out.println(genresToFilter);
+        ArrayList<String> titles = getTitles(genresToFilter);
 
         String[] titlesArray = new String[titles.size()];
         titlesArray = titles.toArray(titlesArray);
@@ -42,8 +45,8 @@ public class Movies {
 
     @GetMapping("/mvfetch/movies-titles-api")
     @ResponseBody
-    public String[] movieTitleAPI() throws Exception {
-        ArrayList<String> posters = getPosters();
+    public String[] movieTitleAPI(String genresToFilter) throws Exception {
+        ArrayList<String> posters = getPosters(genresToFilter);
 
         String[] postersArray = new String[posters.size()];
         postersArray = posters.toArray(postersArray);
@@ -51,16 +54,15 @@ public class Movies {
         return postersArray;
     }
 
-
-    public JSONObject reqResult() throws Exception {
-        String url = "https://api.themoviedb.org/3/discover/movie?api_key=ba188e9dabb5a98c2f73b97d834e220b&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate&with_genres=16&with_genres=35";
+    public JSONObject reqResult(String genresToFilter) throws Exception {
+        String url = "https://api.themoviedb.org/3/discover/movie?api_key=ba188e9dabb5a98c2f73b97d834e220b&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate" + genresToFilter;
         String json = new Scanner(new URL(url).openStream(), "UTF-8").useDelimiter("\\A").next();
         return new JSONObject(json);
     }
 
-    public ArrayList<String> getTitles() throws Exception {
+    public ArrayList<String> getTitles(String genresToFilter) throws Exception {
         ArrayList<String> titles = new ArrayList<String>();
-        JSONObject data = reqResult();
+        JSONObject data = reqResult(genresToFilter);
 
         for (int i = 0; i<data.getJSONArray("results").length(); i++) {
             JSONObject movieDetails = data.getJSONArray("results").getJSONObject(i);
@@ -70,9 +72,9 @@ public class Movies {
         return titles;
     }
 
-    public ArrayList<String> getPosters() throws  Exception {
+    public ArrayList<String> getPosters(String genresToFilter) throws  Exception {
         ArrayList<String> posters = new ArrayList<String>();
-        JSONObject data = reqResult();
+        JSONObject data = reqResult(genresToFilter);
 
         for (int i = 0; i<data.getJSONArray("results").length(); i++) {
             JSONObject movieDetails = data.getJSONArray("results").getJSONObject(i);
